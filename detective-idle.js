c965cc1c4cc8b9b36c55363d461df35d4eb972d4
John Fawcett <jrf0110@gmail.com>
2012-04-21 18:39:15 -0500

    Added support for infinite timeout and made it more interoperable with frameworks

--- detective-idle.js
+++ detective-idle.js
@@ -1,14 +1,14 @@
 (function(){
   DetectiveIdle = (function(){
-    var el = document, $;
-    var _addEvent = function(name, action){
-      return ($ != 'undefined' && $ != null) ? $(el).on(name, action) : el.addEventListener(name, action);
-    };
-    var _extend = function(obj1, obj2){
+    var $, $framework = ($ != 'undefined' && $ != null), bindFn = ($framework && $.hasOwnKey('on')) ? 'on' : 'bind'
+    , _addEvent = function(el, name, action){
+      return $framework ? $(el)._on(name, action) : el.addEventListener(name, action);
+    }
+    , _extend = function(obj1, obj2){
       var obj = {};
       for (var key in obj1){
         if (obj2.hasOwnProperty(key)){
-          if (typeof obj1[key] == "object") obj[key] = _extend(obj1[key], obj2[key]);
+          if (typeof obj1[key] == "object" && key != "el") obj[key] = _extend(obj1[key], obj2[key]);
           else obj[key] = obj2[key];
         }else{
           obj[key] = obj1[key];
@@ -18,11 +18,12 @@
     };
     var defaults = {
       idleInterval: 60000
+    , el: document
     , monitoredEvents: {
         mousemove: true
       , click: true
       , touchmove: true
-      , touhstart: true
+      , touchstart: true
       , keypress: true
       }
     };
@@ -35,70 +36,15 @@
       this.events = {};
       this.eventTimes = [];
       this.eventTimesFired = [];
-      var self = this, boundReset = function(){ self.reset(); };
-
-      for (var key in this.options.monitoredEvents){
-        this.options.monitoredEvents[key] && _addEvent(key, boundReset);
-      }
-
-      console.log(this);
-
+      var self = this, boundReset = function(e){ self.reset(e); };
+      for (var key in this.options.monitoredEvents)
+        this.options.monitoredEvents[key] && _addEvent(this.options.el, key, boundReset);
       return this;
     };
     constructor.prototype = {
-      reset: function(){
-        this.idle = 0;
-        if (this.eventTimesFired.length > 0){
-          this.eventTimes = this.eventTimes.concat(this.eventTimesFired);
-          this.eventTimesFired = [];
-        }
-        return this;
-      }
-    , at: function(time, action){
-        this.on(time + "", action);
-        typeof time != "string" && this.eventTimes.push(time); // just a normal on function at this point
-        return this;
-      }
-    , on: function(name, action){
-        if (Object.prototype.toString.call(action)[8] != "F") throw new TypeError;
-        if (this.events.hasOwnProperty(name)){
-          this.events[name].push(action);
-        }else{
-          this.events[name] = [action];
-        }
-        return this;
-      }
-    , trigger: function(name, args){
-        if (this.events.hasOwnProperty(name)){
-          var e = this.events[name], i = 0;
-          for (; i < e.length; i++){
-            e[i].apply(this, args);
-          }
-        }
-        return this;
-      }
-    , triggerWarning: function(i){
-        this.trigger(this.eventTimes[i] + "", this);
-        this.eventTimesFired.push(this.eventTimes[i]);
-        // Array Remove - By John Resig (MIT Licensed)
-        var rest = this.eventTimes.slice(i + 1);
-        this.eventTimes.length = i;
-        this.eventTimes.push.apply(this.eventTimes, rest);
-        return this;
-      }
-    , onTick: function(){
-        this.idle += this.idleInterval;
-        // Fire off any events bound to specific times
-        for (var i = 0; i < this.eventTimes.length; i++)
-          this.idle >= this.eventTimes[i] && this.triggerWarning(i);
-        if (this.idle >= this.timeout){
-          this.stop();
-          this.trigger('timeout');
-        }
-    }
-    , start: function(){
+      start: function(){
         var self = this;
-        !this.interval && (this.interval = setInterval(function(){self.onTick()}, this.idleInterval));
+        !this.interval && (this.interval = setInterval(function(){self._onTick()}, this.idleInterval));
         return this;
       }
     , stop: function(){
@@ -107,6 +53,55 @@
         this.reset();
         return this;
       }
+    , reset: function(){
+        this.idle = 0;
+        if (this.eventTimesFired.length > 0){
+          this.eventTimes = this.eventTimes.concat(this.eventTimesFired);
+          this.eventTimesFired = [];
+        }
+        return this;
+      }
+    , at: function(time, action){
+        return this.on(time, action);
+      }
+    , on: function(time, action){
+        this._on(time + "", action);
+        typeof time != "string" && this.eventTimes.push(time); // just a normal on function at this point
+        return this;
+      }
+    , _on: function(name, action){
+        if (Object.prototype.toString.call(action)[8] != "F") throw new TypeError;
+        if (this.events.hasOwnProperty(name)){
+          this.events[name].push(action);
+        }else{
+          this.events[name] = [action];
+        }
+        return this;
+      }
+    , _trigger: function(name, args){
+        if (this.events.hasOwnProperty(name))
+          for (var e = this.events[name], i = 0; i < e.length; i++) e[i].apply(this, args);
+        return this;
+      }
+    , _triggerWarning: function(i){
+        this._trigger(this.eventTimes[i] + "", this);
+        this.eventTimesFired.push(this.eventTimes[i]);
+        // Array Remove - By John Resig (MIT Licensed)
+        var rest = this.eventTimes.slice(i + 1);
+        this.eventTimes.length = i;
+        this.eventTimes.push.apply(this.eventTimes, rest);
+        return this;
+      }
+    , _onTick: function(){
+        this.idle += this.idleInterval;
+        // Fire off any events bound to specific times
+        for (var i = 0; i < this.eventTimes.length; i++)
+          this.idle >= this.eventTimes[i] && this._triggerWarning(i);
+        if (this.idle >= this.timeout && this.idle != -1){
+          this.stop();
+          this._trigger('timeout');
+        }
+    }
     };
     return constructor;
   })();
